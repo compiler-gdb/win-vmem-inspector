bf09e5ce368b279aacab1b31f54ec1ccec18017f
compiler-gdb <hwan9971@naver.com>
2026-07-04 17:51:49 +0900

    feat: query virtual memory page states using VirtualQuery API

---
+++ 03-vmem-layout-query/src/03_WinMemState.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <windows.h>
+
+void mem_value(SYSTEM_INFO* si){
+    MEMORY_BASIC_INFORMATION mbi;
+
+    // 탐색할 시작 주소와 끝 주소 설정
+    unsigned char* addr_min = (unsigned char*)si->lpMinimumApplicationAddress;
+    unsigned char* addr_max = (unsigned char*)si->lpMaximumApplicationAddress;
+
+    // 시작 주소부터 끝 주소까지 탐색
+    while(addr_min <= addr_max){
+        if(VirtualQuery(addr_min, &mbi, sizeof(mbi)) == sizeof(mbi)){
+            // 현재 영역의 State를 출력
+            printf("Address: %p | State: %lu\n", (void*)addr_min, mbi.State);
+            
+            addr_min += mbi.RegionSize; //하나의 구역에 State는 모두 동일합니다.
+        }else{
+            // 만약 특정 주소에서 실패할 경우, 다음 페이지(보통 4KB)로 강제 이동하여 계속 탐색
+            addr_min += si->dwPageSize; 
+        }
+    }
+}
+
+int main(void){
+    SYSTEM_INFO si;
+    GetSystemInfo(&si);
+
+    mem_value(&si);
+
+    return 0;
+}
