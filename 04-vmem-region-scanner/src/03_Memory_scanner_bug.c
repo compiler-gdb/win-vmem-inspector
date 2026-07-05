1962ee4dcf0ca91217ce14e5cf0395f3fa910cf0
compiler-gdb <hwan9971@naver.com>
2026-07-05 23:46:42 +0900

    feat: Waste of resources

--- 04-vmem-region-scanner/src/03_Memory_scanner_bug.c
+++ 04-vmem-region-scanner/src/03_Memory_scanner_bug.c
@@ -13,7 +13,6 @@ const char* GetStateString(DWORD state) {
 }
 
 void print_region_state(unsigned char* addr, MEMORY_BASIC_INFORMATION* mbi){ 
-    //MEMORY_BASIC_INFORMAION* mbi - *가 있는 이유는 호출할 때마다 메모리 구조체 데이터를 계속 들고오는 것이 아니라 데이터의 주소만 알려주게 됩니다.
     //출력하는 역할인데 call by Value를 할 필요는 없습니다.
     printf("%p: %-12s", addr, GetStateString(mbi->State)); 
     // mbi -> State mbi 주소를 따라가서 State의 값을 가져와라. mbi에 주소만 넘겼기에 가능한 규칙입니다.
@@ -66,4 +65,3 @@ int main(void){
 
     return 0;
 }
-
